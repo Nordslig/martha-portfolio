800f310c892ebde2cd62048d406d83674ff7ad19
Bartosz Pindel <bartos.pindel@gmail.com>
2022-09-15 01:15:34 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from "./components/navigation/NavBar";
 
 import logo from "./img/logo.png";
 
-import "./App.css";
+import classes from "./App.module.css";
 
 function App() {
   const [actualPage, setActualPage] = useState("main");
@@ -15,19 +15,18 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div style={{ minHeight: "150vh" }}>
+      <header>
         <img src={logo} alt="logo" width="auto" height="auto" />
       </header>
       <NavBar onNavClick={navClickHandler} />
       <CurrentPage page={actualPage} />
-      <footer>
-        <a href="tel:+532917446">532 971 446</a>
-        <a href="mailto: martu.matuszczyk@gmail.com">
-          martu.matuszczyk@gmail.com
-        </a>
+      <footer className={classes["contact-footer"]}>
         <a href="https://www.instagram.com/marthano1/">Instagram</a>
         <a href="https://www.facebook.com/marthano1marthano1">Facebook</a>
+        <p>&#169;Copyrights to Martha Matuszczyk-Romańska</p>
+        <a href="tel:+532917446">Zadzwoń do mnie</a>
+        <a href="mailto: martu.matuszczyk@gmail.com">Napisz do mnie</a>
       </footer>
     </div>
   );
